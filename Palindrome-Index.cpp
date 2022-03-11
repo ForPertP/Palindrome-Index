eb103ab50d5cf9c7c505ef0f43f723150821d6b4
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-03-11 11:19:08 +0900

    Update Palindrome-Index.cpp

--- Palindrome-Index.cpp
+++ Palindrome-Index.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'palindromeIndex' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int palindromeIndex(string s)
+{
+    return 0;
+}
