d955a613644f257196717a175bfe5b341206364b
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-12-23 16:14:04 +0900

    Update Palindrome-Index.cs

--- Palindrome-Index.cs
+++ Palindrome-Index.cs
@@ -11,3 +11,15 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+
+
+class Result
+{
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+}
