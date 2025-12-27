3c7ef0a16f59a9115cd644379996826233158da5
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-12-27 19:38:22 +0900

    Update Palindrome-Index.java

--- Palindrome-Index.java
+++ Palindrome-Index.java
@@ -9,3 +9,13 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+}
