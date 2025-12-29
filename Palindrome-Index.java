920a09633dad9224033498d535e7024311eecfd8
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-12-30 01:05:55 +0900

    Update Palindrome-Index.java

--- Palindrome-Index.java
+++ Palindrome-Index.java
@@ -29,4 +29,27 @@ class Result {
         return true;
     }
 
+
+    public static int palindromeIndex(String s)
+    {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r && s.charAt(l) == s.charAt(r))
+        {
+            l++;
+            r--;
+        }
+
+        if (l >= r)
+            return -1;
+
+        if (isPalindrome(s, l + 1, r))
+            return l;
+
+        if (isPalindrome(s, l, r - 1))
+            return r;
+
+        return -1;
+    }
 }
